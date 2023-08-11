4fbfd053e282b4e7501c119f34baae959d9b5f98
James A. Brannan <jamesabrannan@hushmail.com>
2023-08-11 11:23:33 -0400

    Update InstructionsExample.py

--- InstructionsExample.py
+++ InstructionsExample.py
@@ -3,8 +3,12 @@ from dynamicpdf_api.pdf_response import PdfResponse
 from dynamicpdf_api.page_input import PageInput
 from dynamicpdf_api.rgb_color import RgbColor
 from dynamicpdf_api.font import Font
+from dynamicpdf_api.aes256_security import Aes256Security
 from dynamicpdf_api.elements.element_placement import ElementPlacement
 from dynamicpdf_api.elements.page_numbering_element import PageNumberingElement
+from dynamicpdf_api.pdf_resource import PdfResource
+from dynamicpdf_api.html_resource import HtmlResource
+
 
 def TopLevelMetaData(apiKey):
     pdf = Pdf()
@@ -45,6 +49,37 @@ def FontsExample(apiKey, fontPath):
 
     return pdf
 
+def SecurityExample(apiKey, documentPath):
+    fileResource = documentPath
+    userName = "myuser"
+    passWord = "mypassword"
+    pdf = Pdf()
+    pdf.api_key = apiKey
+    resource = PdfResource(documentPath)
+    pdf.add_pdf(resource)
+    sec = Aes256Security(userName, passWord)
+    sec.allow_copy = False
+    sec.allow_print = False
+    pdf.security = sec
+    return pdf
+
+def HtmlExample(apiKey):
+    pdf=Pdf()
+    pdf.api_key=apiKey
+    pdf.add_html("<html><p>This is a test.</p></html>")
+    pdf.add_html(HtmlResource("html.html", "rb"))
+    pdf.add_html("<html><img src='./images/logo.png'></img></html>", "https://www.dynamicpdf.com")
+    return pdf
+
+def MergePdfs(apiKey):
+    pdf=Pdf()
+    pdf.api_key=apiKey
+    resourceOne = PdfResource("C:/temp/merge-pdf/DocumentA.pdf")
+    pdf.add_pdf(resourceOne)
+    pdf.add_image("samples/get-image-info-image-info-endpoint/dynamicpdfLogo.png")
+    resourceTwo = PdfResource("C:/temp/merge-pdf/DocumentB.pdf")
+    pdf.add_pdf(resourceTwo)
+    return pdf
 
 def outputResult(outputPath, fileName, response:PdfResponse) :
     if response.is_successful:
@@ -58,8 +93,10 @@ def outputResult(outputPath, fileName, response:PdfResponse) :
 def run(apiKey):    
     basePathOut = "C:/temp/instructions-example/out/"
     #pdf = TopLevelMetaData(apiKey)
-    pdf = FontsExample(apiKey, "C:/temp/fonts-example/cnr.otf")
-
+    #pdf = FontsExample(apiKey, "C:/temp/fonts-example/cnr.otf")
+    #pdf = SecurityExample(apiKey, "C:/temp/merge-pdf/DocumentA.pdf")
+    #pdf = HtmlExample(apiKey)
+    pdf = MergePdfs(apiKey)
     response = pdf.process() 
     outputResult(basePathOut, "pdf-python-out.pdf", response)
 
