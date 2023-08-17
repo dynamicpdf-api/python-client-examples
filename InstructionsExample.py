b809c6b7d756aa9a45664ee53998c9c3e729a5fa
James A. Brannan <jamesabrannan@hushmail.com>
2023-08-17 13:09:05 -0400

    Update InstructionsExample.py

--- InstructionsExample.py
+++ InstructionsExample.py
@@ -12,6 +12,12 @@ from dynamicpdf_api.html_resource import HtmlResource
 from dynamicpdf_api.form_field import FormField
 from dynamicpdf_api.template import Template
 from dynamicpdf_api.elements.aztec_barcode_element import AztecBarcodeElement
+from dynamicpdf_api.dlex_resource import DlexResource
+from dynamicpdf_api.layout_data_resource import LayoutDataResource
+from dynamicpdf_api.image_resource import ImageResource
+import json
+import re
+import io
 
 def TopLevelMetaData(apiKey):
     pdf = Pdf()
@@ -156,6 +162,103 @@ def BarcodeExample(apiKey):
     input.template = template
     return pdf
 
+def DlexPdfExample(apiKey):
+    pdf = Pdf()
+    pdf.api_key = apiKey
+    layout = LayoutDataResource("c:/temp/dlex-resource/SimpleReportWithCoverPage.json")
+    pdf.add_dlex("samples/dlex-layout/SimpleReportWithCoverPage.dlex", layout)
+    return pdf
+
+def DlexPdfStringExample(apiKey):
+    pdf = Pdf()
+    pdf.api_key = apiKey
+   
+    with open("c:/temp/dlex-resource/SimpleReportWithCoverPage.json","r") as f:
+        fileData = f.read()
+
+    f.close()
+    python_obj = json.loads(fileData)
+    layout = LayoutDataResource(python_obj)
+    pdf.add_dlex("samples/dlex-layout/SimpleReportWithCoverPage.dlex", layout)
+    return pdf
+
+def ImageExample(apiKey):
+    pdf = Pdf()
+    pdf.api_key = apiKey
+
+    #read image as binary stream
+
+    with io.open("c:/temp/users-guide-resources/A.png", 'rb') as f:
+        imageBinary = f.read()
+
+    imageStream = io.BytesIO(imageBinary)
+    f.close()
+    imageResource = ImageResource(imageStream)
+    pdf.add_image(imageResource)
+
+
+    #get image from cloud storage
+
+    pdf.add_image("samples/users-guide-resources/B.png")
+
+    #get image from local file
+
+    imageFile = "c:/temp/users-guide-resources/C.png"
+    imageResource2 = ImageResource(imageFile)
+    pdf.add_image(imageResource2)
+
+    return pdf
+
+def HtmlExample(apiKey):
+    pdf = Pdf()
+    pdf.api_key = apiKey
+    pdf.add_html("<html><p>Welcome to DynamicPDF Cloud API.</p></html>")
+
+    with open("c:/temp/users-guide-resources/products.html","rt") as f:
+        fileData = f.read()
+    f.close()
+
+    pdf.add_html(fileData)
+    pdf.add_html("<html><img src='./images/logo.png'></img></html>", "https://www.dynamicpdf.com")
+
+    return pdf
+
+
+
+
+def PageExample(apiKey):
+    pdf = Pdf()
+    pdf.api_key = apiKey
+    pageInput = pdf.add_page(1008, 612)
+
+    textElement = TextElement("Hello from DynamicPDF Cloud API.", ElementPlacement.TopCenter)
+    pageNumberingElement = PageNumberingElement("A", ElementPlacement.TopRight)
+    pageNumberingElement.color = RgbColor.red()    
+    pageNumberingElement.font = Font.courier()
+    pageNumberingElement.font_size = 42
+
+    pageInput.elements.append(pageNumberingElement)
+    pageInput.elements.append(textElement)
+
+    return pdf
+
+def PdfExample(apiKey):
+    pdf=Pdf()
+    pdf.api_key=apiKey
+    inputA = pdf.add_pdf(PdfResource("c:/temp/users-guide-resources/DocumentA.pdf"))
+
+  #read pdf as binary stream
+
+    with io.open("c:/temp/users-guide-resources/DocumentB.pdf", 'rb') as f:
+        pdfBinary = f.read()
+
+    pdfStream = io.BytesIO(pdfBinary)
+    f.close()
+
+    pdf.add_pdf(PdfResource(pdfStream))
+    pdf.add_pdf("samples/users-guide-resources/DocumentC.pdf")
+    
+    return pdf
 
 def outputResult(outputPath, fileName, response:PdfResponse) :
     if response.is_successful:
@@ -163,7 +266,6 @@ def outputResult(outputPath, fileName, response:PdfResponse) :
             output_file.write(response.content)
     else:
         print(response.error_json)
-
 
 def run(apiKey):    
     basePathOut = "C:/temp/instructions-example/out/"
@@ -176,7 +278,13 @@ def run(apiKey):
     #pdf = AddOutlinesForNewPdf(apiKey)
     #pdf = AddOutlinesExistingPdf(apiKey)
     #pdf = TemplateExample(apiKey)
-    pdf = BarcodeExample(apiKey)
+    #pdf = BarcodeExample(apiKey)
+    #pdf = DlexPdfExample(apiKey)
+    #pdf = DlexPdfStringExample(apiKey)
+    #pdf = ImageExample(apiKey)
+    #pdf = HtmlExample(apiKey)
+    #pdf = PageExample(apiKey)
+    pdf = PdfExample(apiKey)
 
     response = pdf.process()
     if response.is_successful:
@@ -186,5 +294,5 @@ def run(apiKey):
     
 
 if __name__ == "__main__":
-    api_key = "DP---API-KEY---"
+    api_key = "DP.---API-KEY---"
     run(api_key)
