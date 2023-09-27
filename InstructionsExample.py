c13cfe40d226d2cdbd4dd6816b30536f5f2d1fd1
James A. Brannan <jamesabrannan@hushmail.com>
2023-09-27 14:51:28 -0400

    word input and fixed code

--- InstructionsExample.py
+++ InstructionsExample.py
@@ -15,13 +15,14 @@ from dynamicpdf_api.elements.aztec_barcode_element import AztecBarcodeElement
 from dynamicpdf_api.dlex_resource import DlexResource
 from dynamicpdf_api.layout_data_resource import LayoutDataResource
 from dynamicpdf_api.image_resource import ImageResource
+from dynamicpdf_api.word_input import WordInput
+from dynamicpdf_api.word_resource import WordResource
 import json
 import re
 import io
 
-def TopLevelMetaData(apiKey):
+def TopLevelMetaData():
     pdf = Pdf()
-    pdf.api_key = apiKey
     pdf.add_page(1008, 612)
     pdf.author = "John Doe"
     pdf.keywords ="dynamicpdf api example pdf java instructions"
@@ -30,18 +31,16 @@ def TopLevelMetaData(apiKey):
     pdf.title = "Sample PDF"
     return pdf
 
-def FontsExample(apiKey, fontPath):
+def FontsExample(basePath):
     pdf = Pdf()
-    pdf.api_key = apiKey
     pageInput = pdf.add_page(1008, 612)
-
     pageNumberingElement = PageNumberingElement("A", ElementPlacement.TopRight)
     pageNumberingElement.color = RgbColor.red()    
     pageNumberingElement.font = Font.courier()
     pageNumberingElement.font_size = 42
    
 
-    cloudResourceName = "samples/fonts-example/Calibri.otf"
+    cloudResourceName = "samples/users-guide-resources/Calibri.otf"
     pageNumberingElementTwo = PageNumberingElement("B", ElementPlacement.TopLeft)
     pageNumberingElementTwo.color = RgbColor.dark_orange()
     pageNumberingElementTwo.font = Font(cloudResourceName)
@@ -49,7 +48,7 @@ def FontsExample(apiKey, fontPath):
 
     pageNumberingElementThree = PageNumberingElement("C", ElementPlacement.TopCenter)
     pageNumberingElementThree.color = RgbColor.green()
-    pageNumberingElementThree.font = Font.from_file(fontPath)
+    pageNumberingElementThree.font = Font.from_file(basePath + "cnr.otf")
     pageNumberingElementThree.fontSize = 42
 
     pageInput.elements.append(pageNumberingElement)
@@ -58,13 +57,27 @@ def FontsExample(apiKey, fontPath):
 
     return pdf
 
-def SecurityExample(apiKey, documentPath):
+def WordExample(documentPath):
+    pdf=Pdf()
     fileResource = documentPath
+    wordResource = WordResource(documentPath + "Doc1.docx")       
+    word = WordInput(wordResource)
+    word.page_width = 300
+    word.page_height = 200
+
+    word.top_margin = 10
+    word.bottom_margin = 10
+    word.right_margin = 40
+    word.left_margin = 40
+    pdf.inputs.append(word)
+    return pdf
+
+def SecurityExample(documentPath):
+    fileResource = documentPath + "DocumentB.pdf"
     userName = "myuser"
     passWord = "mypassword"
     pdf = Pdf()
-    pdf.api_key = apiKey
-    resource = PdfResource(documentPath)
+    resource = PdfResource(fileResource)
     pdf.add_pdf(resource)
     sec = Aes256Security(userName, passWord)
     sec.allow_copy = False
@@ -72,27 +85,29 @@ def SecurityExample(apiKey, documentPath):
     pdf.security = sec
     return pdf
 
-def HtmlExample(apiKey):
-    pdf=Pdf()
-    pdf.api_key=apiKey
-    pdf.add_html("<html><p>This is a test.</p></html>")
-    pdf.add_html(HtmlResource("html.html", "rb"))
+def HtmlExample(documentPath):
+    pdf = Pdf()
+    pdf.add_html("<html><p>Welcome to DynamicPDF Cloud API.</p></html>")
+    
+    with open(documentPath + "products.html","rt") as f:
+        fileData = f.read()
+        f.close()
+
+    pdf.add_html(fileData)
     pdf.add_html("<html><img src='./images/logo.png'></img></html>", "https://www.dynamicpdf.com")
     return pdf
 
-def MergePdfs(apiKey):
+def MergePdfs(documentPath):
     pdf=Pdf()
-    pdf.api_key=apiKey
-    resourceOne = PdfResource("C:/temp/merge-pdf/DocumentA.pdf")
+    resourceOne = PdfResource(documentPath + "DocumentA.pdf")
     pdf.add_pdf(resourceOne)
     pdf.add_image("samples/get-image-info-image-info-endpoint/dynamicpdfLogo.png")
-    resourceTwo = PdfResource("C:/temp/merge-pdf/DocumentB.pdf")
+    resourceTwo = PdfResource(documentPath + "DocumentB.pdf")
     pdf.add_pdf(resourceTwo)
     return pdf
 
-def AcroFormExample(apiKey):
+def AcroFormExample():
     pdf=Pdf()
-    pdf.api_key = apiKey
     pdf.add_pdf("samples/users-guide-resources/simple-form-fill.pdf");
     formField = FormField("nameField", "DynamicPDF");
     formField2 = FormField("descriptionField", "DynamicPDF CloudAPI. RealTime PDFs, Real FAST!");
@@ -100,11 +115,8 @@ def AcroFormExample(apiKey):
     pdf.form_fields.append(formField2)
     return pdf
 
-
-
-def AddOutlinesForNewPdf(apiKey):
+def AddOutlinesForNewPdf():
     pdf = Pdf()
-    pdf.api_key = apiKey
     pdf.author = "John Doe"
     pdf.title = "Sample Pdf"
     pageInput = pdf.add_page()
@@ -121,15 +133,14 @@ def AddOutlinesForNewPdf(apiKey):
     rootOutline.children.add("Page 3", pageInput2)
     return pdf
 
-def AddOutlinesExistingPdf(apiKey):
+def AddOutlinesExistingPdf(documentPath):
     pdf = Pdf()
-    pdf.api_key = apiKey
     pdf.author = "John Doe"
     pdf.title = "Sample Pdf"
-    resource = PdfResource("C:/temp/users-guide-resources/AllPageElements.pdf")
+    resource = PdfResource(documentPath + "AllPageElements.pdf")
     input = pdf.add_pdf(resource)
     input.id = "AllPageElements"
-    resource1 = PdfResource("C:/temp/users-guide-resources/OutlineExisting.pdf")
+    resource1 = PdfResource(documentPath + "OutlineExisting.pdf")
     input1 = pdf.add_pdf(resource1)
     input1.id = "outlineDoc1"
 
@@ -139,10 +150,9 @@ def AddOutlinesExistingPdf(apiKey):
     rootOutline.children.add(input1)
     return pdf
 
-def TemplateExample(apiKey):
+def TemplateExample(documentPath):
     pdf = Pdf()
-    pdf.api_key = apiKey
-    resource = PdfResource("C:/temp/merge-pdf/DocumentA.pdf")
+    resource = PdfResource(documentPath + "DocumentA.pdf")
     input = pdf.add_pdf(resource)
 
     template = Template("Temp1")
@@ -151,10 +161,9 @@ def TemplateExample(apiKey):
     input.template = template
     return pdf
 
-def BarcodeExample(apiKey):
+def BarcodeExample(documentPath):
     pdf = Pdf()
-    pdf.api_key = apiKey
-    resource = PdfResource("C:/temp/merge-pdf/DocumentA.pdf")
+    resource = PdfResource(documentPath + "DocumentA.pdf")
     input = pdf.add_pdf(resource)
     template = Template("Temp1")
     element = AztecBarcodeElement("Hello World", ElementPlacement.TopCenter, 0, 500)
@@ -162,33 +171,30 @@ def BarcodeExample(apiKey):
     input.template = template
     return pdf
 
-def DlexPdfExample(apiKey):
+def DlexPdfExample(documentPath):
     pdf = Pdf()
-    pdf.api_key = apiKey
-    layout = LayoutDataResource("c:/temp/dlex-resource/SimpleReportWithCoverPage.json")
-    pdf.add_dlex("samples/dlex-layout/SimpleReportWithCoverPage.dlex", layout)
+    layout = LayoutDataResource(documentPath + "SimpleReportWithCoverPage.json")
+    pdf.add_dlex("samples/users-guide-resources/SimpleReportWithCoverPage.dlex", layout)
     return pdf
 
-def DlexPdfStringExample(apiKey):
+def DlexPdfStringExample(documentPath):
     pdf = Pdf()
-    pdf.api_key = apiKey
    
-    with open("c:/temp/dlex-resource/SimpleReportWithCoverPage.json","r") as f:
+    with open(documentPath + "SimpleReportWithCoverPage.json","r") as f:
         fileData = f.read()
 
     f.close()
     python_obj = json.loads(fileData)
     layout = LayoutDataResource(python_obj)
-    pdf.add_dlex("samples/dlex-layout/SimpleReportWithCoverPage.dlex", layout)
+    pdf.add_dlex("samples/users-guide-resources/SimpleReportWithCoverPage.dlex", layout)
     return pdf
 
-def ImageExample(apiKey):
+def ImageExample(documentPath):
     pdf = Pdf()
-    pdf.api_key = apiKey
 
     #read image as binary stream
 
-    with io.open("c:/temp/users-guide-resources/A.png", 'rb') as f:
+    with io.open(documentPath + "A.png", 'rb') as f:
         imageBinary = f.read()
 
     imageStream = io.BytesIO(imageBinary)
@@ -203,18 +209,17 @@ def ImageExample(apiKey):
 
     #get image from local file
 
-    imageFile = "c:/temp/users-guide-resources/C.png"
+    imageFile = documentPath + "C.png"
     imageResource2 = ImageResource(imageFile)
     pdf.add_image(imageResource2)
 
     return pdf
 
-def HtmlExample(apiKey):
+def HtmlExample(documentPath):
     pdf = Pdf()
-    pdf.api_key = apiKey
     pdf.add_html("<html><p>Welcome to DynamicPDF Cloud API.</p></html>")
 
-    with open("c:/temp/users-guide-resources/products.html","rt") as f:
+    with open(documentPath + "products.html","rt") as f:
         fileData = f.read()
     f.close()
 
@@ -223,12 +228,8 @@ def HtmlExample(apiKey):
 
     return pdf
 
-
-
-
-def PageExample(apiKey):
+def PageExample():
     pdf = Pdf()
-    pdf.api_key = apiKey
     pageInput = pdf.add_page(1008, 612)
 
     textElement = TextElement("Hello from DynamicPDF Cloud API.", ElementPlacement.TopCenter)
@@ -242,14 +243,13 @@ def PageExample(apiKey):
 
     return pdf
 
-def PdfExample(apiKey):
+def PdfExample(documentPath):
     pdf=Pdf()
-    pdf.api_key=apiKey
-    inputA = pdf.add_pdf(PdfResource("c:/temp/users-guide-resources/DocumentA.pdf"))
+    inputA = pdf.add_pdf(PdfResource(documentPath + "DocumentA.pdf"))
 
   #read pdf as binary stream
 
-    with io.open("c:/temp/users-guide-resources/DocumentB.pdf", 'rb') as f:
+    with io.open(documentPath + "DocumentB.pdf", 'rb') as f:
         pdfBinary = f.read()
 
     pdfStream = io.BytesIO(pdfBinary)
@@ -267,32 +267,54 @@ def outputResult(outputPath, fileName, response:PdfResponse) :
     else:
         print(response.error_json)
 
-def run(apiKey):    
-    basePathOut = "C:/temp/instructions-example/out/"
-    #pdf = TopLevelMetaData(apiKey)
-    #pdf = FontsExample(apiKey, "C:/temp/fonts-example/cnr.otf")
-    #pdf = SecurityExample(apiKey, "C:/temp/merge-pdf/DocumentA.pdf")
-    #pdf = HtmlExample(apiKey)
-    #pdf = MergePdfs(apiKey)
-    #pdf = AcroFormExample(apiKey)
-    #pdf = AddOutlinesForNewPdf(apiKey)
-    #pdf = AddOutlinesExistingPdf(apiKey)
-    #pdf = TemplateExample(apiKey)
-    #pdf = BarcodeExample(apiKey)
-    #pdf = DlexPdfExample(apiKey)
-    #pdf = DlexPdfStringExample(apiKey)
-    #pdf = ImageExample(apiKey)
-    #pdf = HtmlExample(apiKey)
-    #pdf = PageExample(apiKey)
-    pdf = PdfExample(apiKey)
-
+def outputPdf(pdf:Pdf, apiKey, baseOutputPath, outputFile):
+    pdf.api_key = apiKey
     response = pdf.process()
     if response.is_successful:
-        outputResult(basePathOut, "pdf-python-out.pdf", response)
+        outputResult(baseOutputPath, outputFile, response)
     else:
+        print(response.error_message)
         print(response.error_json) 
+
+def run(apiKey):    
+    basePathOut = "c:/temp/dynamicpdf-api-usersguide-examples/python-output/"
+    basePathIn = "c:/temp/users-guide-resources/"
+    pdf = TopLevelMetaData()
+    outputPdf(pdf, apiKey, basePathOut, "top-level-metadata-out.pdf")
+    pdf = FontsExample(basePathIn)
+    outputPdf(pdf, apiKey, basePathOut, "fonts-out.pdf")
+    pdf = SecurityExample(basePathIn)
+    outputPdf(pdf, apiKey, basePathOut, "security-out.pdf")
+    pdf = HtmlExample(basePathIn)
+    outputPdf(pdf, apiKey, basePathOut, "html-out.pdf")
+    pdf = MergePdfs(basePathIn)
+    outputPdf(pdf, apiKey, basePathOut, "merge-out.pdf")
+    pdf = AcroFormExample()
+    outputPdf(pdf, apiKey, basePathOut, "acroform-out.pdf")
+    pdf = AddOutlinesForNewPdf()
+    outputPdf(pdf, apiKey, basePathOut, "outlines-out.pdf")
+    pdf = AddOutlinesExistingPdf(basePathIn)
+    outputPdf(pdf, apiKey, basePathOut, "outlines-existing-out.pdf")
+    pdf = TemplateExample(basePathIn)
+    outputPdf(pdf, apiKey, basePathOut, "template-out.pdf")
+    pdf = BarcodeExample(basePathIn)
+    outputPdf(pdf, apiKey, basePathOut, "barcode-out.pdf")
+    pdf = DlexPdfExample(basePathIn)
+    outputPdf(pdf, apiKey, basePathOut, "dlex-out.pdf")
+    pdf = DlexPdfStringExample(basePathIn)
+    outputPdf(pdf, apiKey, basePathOut, "dlex-string-out.pdf")
+    pdf = ImageExample(basePathIn)
+    outputPdf(pdf, apiKey, basePathOut, "top-level-metadata-out.pdf")
+    pdf = HtmlExample(basePathIn)
+    outputPdf(pdf, apiKey, basePathOut, "image-out.pdf")
+    pdf = PageExample()
+    outputPdf(pdf, apiKey, basePathOut, "page-out.pdf")
+    pdf = PdfExample(basePathIn)
+    outputPdf(pdf, apiKey, basePathOut, "pdf-out.pdf")
+    pdf = WordExample(basePathIn)
+    outputPdf(pdf, apiKey, basePathOut, "word-out.pdf")
     
 
 if __name__ == "__main__":
-    api_key = "DP.---API-KEY---"
+    api_key = "DP---API-KEY---"
     run(api_key)
