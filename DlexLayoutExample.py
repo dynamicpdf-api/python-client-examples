ad4f972947a421fd0eb22e7ea6e82d508379a2fd
James A. Brannan <jamesabrannan@hushmail.com>
2023-10-24 15:31:43 -0400

    Update DlexLayoutExample.py

--- DlexLayoutExample.py
+++ DlexLayoutExample.py
@@ -1,17 +1,18 @@
 from dynamicpdf_api.dlex_layout import DlexLayout
 from dynamicpdf_api.layout_data_resource import LayoutDataResource
 
-def dlex_layout(apiKey):
-    layoutData = LayoutDataResource("C:/temp/dlex-layout-example/SimpleReportWithCoverPage.json")
-    dlexEndpoint =DlexLayout("samples/dlex-layout/SimpleReportWithCoverPage.dlex", layoutData)
+def dlex_layout(apiKey, basePath):
+    layoutData = LayoutDataResource(basePath + "creating-pdf-dlex-layout.json")
+    dlexEndpoint =DlexLayout("samples/creating-pdf-dlex-layout-endpoint/creating-pdf-dlex-layout.dlex", layoutData)
     dlexEndpoint.api_key=apiKey
     response = dlexEndpoint.process() 
     if response.is_successful:
-         with open("C:/temp/dlex-layout-example/python-dlex-layout-example.pdf", "wb") as output_file:
+         with open(basePath + "python-dlex-layout-example.pdf", "wb") as output_file:
             output_file.write(response.content)
     else:
         print(response.error_id)
     
 # Call the function
-api_key = 'DP---API-KEY---'
-dlex_layout(api_key)
+api_key = 'DP.25DHurNAMB8MEgzPg3mmUyBsjkqQbjgVAZuFuu4ynh6OSaBCOp6JIrR7'
+basePath = "C:/temp/dynamicpdf-api-samples/"
+dlex_layout(api_key, basePath)
