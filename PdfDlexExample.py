1792715d42fcd0f593e6f72d6fdb0548b2206462
James A. Brannan <jamesabrannan@hushmail.com>
2023-10-24 15:53:16 -0400

    Create PdfDlexExample.py

---
+++ PdfDlexExample.py
@@ -0,0 +1,24 @@
+from dynamicpdf_api.pdf import Pdf
+from dynamicpdf_api.page_input import PageInput
+from dynamicpdf_api.layout_data_resource import LayoutDataResource
+
+def run(apiKey, basePath):
+    
+    pdf=Pdf()
+    pdf.api_key=apiKey
+
+    layout = LayoutDataResource(basePath + "SimpleReportWithCoverPage.json")
+    pdf.add_dlex("samples/creating-pdf-pdf-endpoint/SimpleReportWithCoverPage.dlex", layout)
+
+    response = pdf.process() 
+
+    if response.is_successful:
+         with open(basePath + "pdf-dlex-output-python.pdf", "wb") as output_file:
+            output_file.write(response.content)
+    else:
+        print(response.error_id)
+    
+# Call the function
+api_key = 'DP.25DHurNAMB8MEgzPg3mmUyBsjkqQbjgVAZuFuu4ynh6OSaBCOp6JIrR7'
+basePath = "C:/temp/dynamicpdf-api-samples/"
+run(api_key, basePath)
