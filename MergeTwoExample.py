5de3aea02e144fa5419d5350bdac6c9d4a58491a
James A. Brannan <jamesabrannan@hushmail.com>
2023-11-20 12:10:36 -0500

    Create MergeTwoExample.py

---
+++ MergeTwoExample.py
@@ -0,0 +1,19 @@
+from dynamicpdf_api.pdf import Pdf
+from dynamicpdf_api.pdf_resource import PdfResource
+
+def merge_pdfs():
+    basePath = "C:/temp/dynamicpdf-api-samples/"
+    pdf.api_key="DP.25DHurNAMB8MEgzPg3mmUyBsjkqQbjgVAZuFuu4ynh6OSaBCOp6JIrR7"
+    pdf=Pdf()
+    inputA = pdf.add_pdf(PdfResource(basePath + "DocumentA.pdf"))
+    pdf.add_pdf(PdfResource(basePath + "DocumentB.pdf"))
+    response = pdf.process() 
+    if response.is_successful:
+         with open(basePath + "merge-pdfs-output-python.pdf", "wb") as output_file:
+            output_file.write(response.content)
+    else:
+        print(response.error_id)
+        print(response.error_message)
+        print(response.error_json)
+
+merge_pdfs()
