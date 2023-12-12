ef9603711b755b7594e81c7201c36a09c469a97d
James A. Brannan <jamesabrannan@hushmail.com>
2023-12-12 13:14:51 -0500

    removed apikey

--- Shared.py
+++ Shared.py
@@ -1,4 +1,4 @@
-api_key = 'DP.Tai26vtanTPXDVJz6Y1u0wOYAP6Mv3U/yOUU4wg1+2DtTJuCnCXz7NB1'
+api_key = 'DP.---api-key---'
 base_path = "C:/temp/dynamicpdf-api-samples/"
 output_path = "c:/temp/dynamicpdf-api-samples/users-guide-output/";
 users_guide_resource_path = "c:/temp/dynamicpdf-api-samples/users-guide-resources/";
