5e2c8f83a625b3daea5066b0fcf8c85aba326237
James A. Brannan <jamesabrannan@hushmail.com>
2024-04-16 12:24:02 -0400

    added excel example

---
+++ ExcelExample.py
@@ -0,0 +1,26 @@
+from dynamicpdf_api.pdf import Pdf
+from dynamicpdf_api.excel_input import ExcelInput
+from dynamicpdf_api.excel_resource import ExcelResource
+
+from Shared import *
+
+def excel_example(apikey, documentPath, outputPath):
+    
+    pdf=Pdf()
+    pdf.api_key=apikey
+    fileResource = documentPath
+    excelResource = ExcelResource(documentPath + "sample-data.xlsx")       
+    excel = ExcelInput(excelResource)
+    
+    pdf.inputs.append(excel)
+
+    response = pdf.process() 
+
+    if response.is_successful:
+         with open(output_path + "pdf-excel-output-python.pdf", "wb") as output_file:
+            output_file.write(response.content)
+    else:
+        print(response.error_id)
+    
+if __name__ == "__main__":
+    excel_example(api_key, "./resources/users-guide/", "./output/")
--- Shared.py
+++ Shared.py
@@ -1,3 +1,3 @@
-api_key = 'DP---api-key---'
+api_key = 'DP--api-key--'
 base_path = "./resources/"
 output_path = "./output/"
