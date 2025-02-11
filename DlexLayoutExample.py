285b348cf1c346f5daa725b319acdb510a3aaa2c
James A. Brannan <jamesabrannan@hushmail.com>
2025-02-11 14:23:57 -0500

    added example

--- DlexLayoutExample.py
+++ DlexLayoutExample.py
@@ -5,6 +5,7 @@ from Shared import *
 
 def dlex_layout(apiKey, full_path):
     dlex_layout_cloud(apiKey, full_path)
+    dlex_layout_local_two(apiKey, full_path)
     dlex_layout_local(apiKey, full_path)
 
 def dlex_layout_cloud(apiKey, full_path):
@@ -18,7 +19,7 @@ def dlex_layout_cloud(apiKey, full_path):
     else:
         print(response.error_id)
 
-def dlex_layout_local(apiKey, full_path):
+def dlex_layout_local_two(apiKey, full_path):
     layoutData = LayoutDataResource(full_path + "creating-pdf-dlex-layout.json")
     dlexResource = DlexResource(full_path + "creating-pdf-dlex-layout.dlex", "creating-pdf-dlex-layout.dlex")
     dlexEndpoint =DlexLayout(dlexResource, layoutData)
@@ -26,10 +27,24 @@ def dlex_layout_local(apiKey, full_path):
     dlexEndpoint.api_key=apiKey
     response = dlexEndpoint.process() 
     if response.is_successful:
+         with open(output_path + "python-dlex-layout-two-local-example.pdf", "wb") as output_file:
+            output_file.write(response.content)
+    else:
+        print(response.error_id)
+
+def dlex_layout_local(apiKey, full_path):
+    layoutData = LayoutDataResource(full_path + "ExampleTemplate.json")
+    dlexResource = DlexResource(full_path + "ExampleTemplate.dlex", "ExampleTemplate.dlex")
+    dlexEndpoint =DlexLayout(dlexResource, layoutData)
+    dlexEndpoint.add_additional_resource(full_path + "signature-one.png")
+    dlexEndpoint.add_additional_resource(full_path + "template_example.pdf")
+    dlexEndpoint.api_key=apiKey
+    response = dlexEndpoint.process() 
+    if response.is_successful:
          with open(output_path + "python-dlex-layout-local-example.pdf", "wb") as output_file:
             output_file.write(response.content)
     else:
         print(response.error_id)
-    
+
 if __name__ == "__main__":
     dlex_layout(api_key, base_path + "/creating-pdf-dlex-layout/")
